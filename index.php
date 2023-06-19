80971613d63e346f1a29c5d90852e68584e00260
Trinkix <jonathan.somny@pacificproweb.nc>
2023-06-19 14:29:10 +1100

    feat : test

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php
+// Create a new SimpleXMLElement object
+$xml = new SimpleXMLElement('<root/>');
+
+// Add a new child to the XML object
+$title = $xml->addChild('title', 'This is the title');
+
+// Save the XML content to a string
+$xmlContent = $xml->asXML();
+
+// Print the XML content
+echo $xmlContent;
