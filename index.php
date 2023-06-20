aba24ac56c45799816beac92cf98e1ed513ae714
Trinkix <jonathan.somny@pacificproweb.nc>
2023-06-20 18:15:55 +1100

    feat : pire ecf de la terre

--- index.php
+++ index.php
@@ -1,50 +1,111 @@
+<?php
+include 'xml.php';
+?>
+
 <!DOCTYPE html>
 <html>
 
 <body>
-    <div id="container">
-        <div>
-            <input type="radio" id="year" name="contact" value="year" onchange="displayForm(this.value)">
-            <label for="email">Annuel</label>
 
-            <input type="radio" id="trimester" name="contact" value="trimester" onchange="displayForm(this.value)">
-            <label for="phone">Trimestriel</label>
+    <form action="#" method="get" id="form_data">
+        <input type="radio" id="year" name="contact" value="ANNUEL" onchange="displayForm(this.value)">
+        <label for="email">Annuel</label>
 
-            <input type="radio" id="mensuel" name="contact" value="mensuel" onchange="displayForm(this.value)">
-            <label for="mail">Mensuel</label>
+        <input type="radio" id="trimester" name="contact" value="TRIMESTRIEL" onchange="displayForm(this.value)">
+        <label for="phone">Trimestriel</label>
+
+        <input type="radio" id="mensuel" name="contact" value="MENSUEL" onchange="displayForm(this.value)">
+        <label for="mail">Mensuel</label>
         </div>
-    </div>
+        <div id="date_data">
+        </div>
 
-    <form action="" id="form_date"></form>
+        <input type="submit" target="_blank" value="Send">
+    </form>
+
+    <script>
+        function displayForm(value) {
+            let form_data = document.getElementById("form_data");
+            let date_data = document.getElementById("date_data");
+
+            date_data.innerHTML = '';
+
+            let inputYear = document.createElement('input');
+            inputYear.type = 'number';
+            inputYear.name = 'year';
+            inputYear.min = "2000";
+            inputYear.max = "3000";
+            inputYear.value = '2020';
+            date_data.appendChild(inputYear);
+
+            if (value === "TRIMESTRIEL") {
+                let inputQuarter = document.createElement('select');
+                inputQuarter.id = "quarter-select"; // Add an id to the select element for easier access
+                inputQuarter.name = "quarter"; // Set the name attribute for the select element
+                for (let i = 1; i < 5; i++) {
+                    let inputQuarterOption = document.createElement('option');
+                    inputQuarterOption.id = "option-" + i;
+                    inputQuarterOption.value = i; // Set the value attribute to the quarter number
+                    inputQuarterOption.innerHTML = 'Q' + i;
+                    inputQuarter.appendChild(inputQuarterOption);
+                }
+                date_data.appendChild(inputQuarter);
+            }
+
+            if (value === "MENSUEL") {
+                let inputMonth = document.createElement('select');
+                inputMonth.name = "month";
+                for (let i = 1; i <= 12; i++) {
+                    let inputMonthOption = document.createElement('option');
+                    inputMonthOption.id = "option-" + i;
+                    inputMonthOption.value = (i < 10 ? '0' : '') + i;
+                    switch (i) {
+                        case 1:
+                            inputMonthOption.innerHTML = "Janvier";
+                            break;
+                        case 2:
+                            inputMonthOption.innerHTML = "Février";
+                            break;
+                        case 3:
+                            inputMonthOption.innerHTML = "Mars";
+                            break;
+                        case 4:
+                            inputMonthOption.innerHTML = "Avril";
+                            break;
+                        case 5:
+                            inputMonthOption.innerHTML = "Mai";
+                            break;
+                        case 6:
+                            inputMonthOption.innerHTML = "Juin";
+                            break;
+                        case 7:
+                            inputMonthOption.innerHTML = "Juillet";
+                            break;
+                        case 8:
+                            inputMonthOption.innerHTML = "Août";
+                            break;
+                        case 9:
+                            inputMonthOption.innerHTML = "Septembre";
+                            break;
+                        case 10:
+                            inputMonthOption.innerHTML = "Octobre";
+                            break;
+                        case 11:
+                            inputMonthOption.innerHTML = "Novembre";
+                            break;
+                        case 12:
+                            inputMonthOption.innerHTML = "Décembre";
+                            break;
+                        default:
+                            break;
+                    }
+                    inputMonth.appendChild(inputMonthOption);
+                }
+                date_data.appendChild(inputMonth);
+            }
+        }
+    </script>
 </body>
 
-<script>
-    function displayForm(value) {
-        let form_date = document.getElementById("form_date");
-        form_date.innerHTML = '';
-
-        let inputYear = document.createElement('input');
-        inputYear.type = 'number';
-        inputYear.name = 'year';
-        inputYear.placeholder = 'Enter year';
-        form_date.appendChild(inputYear);
-
-        if (value === "trimester") {
-            let inputQuarter = document.createElement('input');
-            inputQuarter.type = 'number';
-            inputQuarter.name = 'quarter';
-            inputQuarter.placeholder = 'Enter quarter (1-4)';
-            form_date.appendChild(inputQuarter);
-        }
-
-        if (value === "mensuel") {
-            let inputMonth = document.createElement('input');
-            inputMonth.type = 'number';
-            inputMonth.name = 'month';
-            inputMonth.placeholder = 'Enter month (1-12)';
-            form_date.appendChild(inputMonth);
-        }
-    }
-</script>
 
 </html>
--- xml.php
+++ xml.php
@@ -1,144 +1,285 @@
 <?php
+require 'vendor/autoload.php';
 
-//Create XML
-$doc = new DOMDocument('1.0', 'ISO-8859-1');
-$doc->formatOutput = true;
-
-$root = $doc->createElement('doc');
-$doc->appendChild($root);
-
-//... Add your other XML elements here
-// doc
-$docElement = $doc->createElement('doc');
-$doc->appendChild($docElement);
-
-// entete
-$entete = $doc->createElement('entete');
-$docElement->appendChild($entete);
-
-$entete->appendChild($doc->createElement('type', 'DN'));
-$entete->appendChild($doc->createElement('version', 'VERSION_2_0'));
-$entete->appendChild($doc->createElement('emetteur', 'ATP'));
-$entete->appendChild($doc->createElement('dateGeneration', '2022-10-13T12:00:00'));
-
-$logiciel = $doc->createElement('logiciel');
-$entete->appendChild($logiciel);
-
-$logiciel->appendChild($doc->createElement('editeur', 'MONEDITEUR'));
-$logiciel->appendChild($doc->createElement('nom', 'MONPROGICIEL'));
-$logiciel->appendChild($doc->createElement('version', '11.0.0 Patch 3'));
-$logiciel->appendChild($doc->createElement('dateVersion', '2022-12-25'));
-
-// corps
-$corps = $doc->createElement('corps');
-$docElement->appendChild($corps);
-
-// periode
-$periode = $doc->createElement('periode');
-$corps->appendChild($periode);
-
-$periode->appendChild($doc->createElement('type', 'TRIMESTRIEL'));
-$periode->appendChild($doc->createElement('annee', '2023'));
-$periode->appendChild($doc->createElement('numero', '1'));
-
-// attributs
-$attributs = $doc->createElement('attributs');
-$corps->appendChild($attributs);
-
-$attributs->appendChild($doc->createElement('complementaire', 'false'));
-$attributs->appendChild($doc->createElement('contratAlternance', 'false'));
-$attributs->appendChild($doc->createElement('pasAssureRemunere', 'false'));
-$attributs->appendChild($doc->createElement('pasDeReembauche', 'false'));
-
-// employeur
-$employeur = $doc->createElement('employeur');
-$corps->appendChild($employeur);
-
-$employeur->appendChild($doc->createElement('numero', '123456'));
-$employeur->appendChild($doc->createElement('suffixe', '1'));
-$employeur->appendChild($doc->createElement('nom', 'TENNIS CONSULTING'));
-$employeur->appendChild($doc->createElement('rid', '12345'));
-$employeur->appendChild($doc->createElement('codeCotisation', '001'));
-$employeur->appendChild($doc->createElement('tauxATPrincipal', '0.72'));
-
-// assures
-$assures = $doc->createElement('assures');
-$corps->appendChild($assures);
-
-$assure = $doc->createElement('assure');
-$assures->appendChild($assure);
+use Carbon\Carbon;
 
 
-$assure->appendChild($doc->createElement('numero', '458784'));
-$assure->appendChild($doc->createElement('nom', 'LENDL'));
-$assure->appendChild($doc->createElement('prenoms', 'IVAN DIMITRI'));
-$assure->appendChild($doc->createElement('dateNaissance', '1960-03-07'));
-$assure->appendChild($doc->createElement('codeAT', 'PRINCIPAL'));
-$assure->appendChild($doc->createElement('etablissementRID', '123'));
-$assure->appendChild($doc->createElement('codeCommune', '09'));
-$assure->appendChild($doc->createElement('nombreHeures', '156.00'));
-$assure->appendChild($doc->createElement('remuneration', '500000'));
 
-// assiettes
-$assiettes = $doc->createElement('assiettes');
-$assure->appendChild($assiettes);
+if (isset($_GET['month']) || isset($_GET['quarter']) || isset($_GET['year'])) {
 
-$assiette1 = $doc->createElement('assiette');
-$assiettes->appendChild($assiette1);
+    $now = Carbon::now()->format('Y-m-d\TH:i:s');
 
-$assiette1->appendChild($doc->createElement('type', 'RUAMM'));
-$assiette1->appendChild($doc->createElement('valeur', '500000'));
 
-$assiette2 = $doc->createElement('assiette');
-$assiettes->appendChild($assiette2);
+    $host = 'localhost';
+    $db   = 'ecfc6';
+    $user = 'root';
+    $pass = '';
+    $charset = 'utf8mb4';
 
-$assiette2->appendChild($doc->createElement('type', 'FIAF'));
-$assiette2->appendChild($doc->createElement('valeur', '500000'));
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $opt);
+    } catch (PDOException $e) {
+        throw new PDOException($e->getMessage(), (int)$e->getCode());
+    }
 
-$assure->appendChild($doc->createElement('dateRupture', '2023-01-31'));
-$assure->appendChild($doc->createElement('observations', 'FIN DE CONTRAT'));
+    $societe = "SELECT * FROM societe";
+    $societe = $pdo->query($societe);
+    $societe = $societe->fetch();
 
-// decompte
-$decompte = $doc->createElement('decompte');
-$corps->appendChild($decompte);
+    $societeNumeroCafat = explode('/', $societe['numerocafat']);
+    $societeNumeroRidet = explode('.', $societe['ridet']);
 
-// cotisations
-$cotisations = $doc->createElement('cotisations');
-$decompte->appendChild($cotisations);
 
-$cotisation = $doc->createElement('cotisation');
-$cotisations->appendChild($cotisation);
+    $month = $_GET['month'] ?? "";
+    $quarter = $_GET['quarter'] ?? "";
+    $year = $_GET['year'];
+    $type =  $_GET["contact"];
 
-$cotisation->appendChild($doc->createElement('type', 'RUAMM'));
-$cotisation->appendChild($doc->createElement('tranche', 'TRANCHE_1'));
-$cotisation->appendChild($doc->createElement('assiette', '3583400'));
-$cotisation->appendChild($doc->createElement('valeur', '556144'));
+    if (isset($year)) {
+        // $dateEmbauche = $year;
+        if ($quarter != "") {
+            switch ($quarter) {
+                case 1:
+                    $firstMonth = "01";
+                    $lastMonth = "03";
+                    break;
+                case 2:
+                    $firstMonth = "04";
+                    $lastMonth = "06";
+                    break;
+                case 3:
+                    $firstMonth = "07";
+                    $lastMonth = "09";
+                    break;
+                case 4:
+                    $firstMonth = "10";
+                    $lastMonth = "12";
+                    break;
+                default:
+                    break;
+            }
+            $employees = "SELECT * FROM `salaries` WHERE YEAR(dembauche) = 2022 AND MONTH(dembauche) BETWEEN $firstMonth AND $lastMonth";
 
-$decompte->appendChild($doc->createElement('totalCotisations', '1803203'));
+            $employees = $pdo->query($employees);
+            $employees = $employees->fetchAll();
+        }
 
-// deductions
-$deductions = $doc->createElement('deductions');
-$decompte->appendChild($deductions);
+        if ($month != "") {
+            $yearOnly = $year;
+            $year = $year . $month;
+            $stmt = $pdo->prepare("SELECT 
+            salaries.id AS salarie_id, salaries.nom, salaries.prenom,salaries.numcafat,salaries.dnaissance,salaries.dembauche,
+            SUM(bulletin.brut) AS total_brut,
+            SUM(bulletin.nombre_heures) AS total_hours
+            FROM 
+                bulletin
+            INNER JOIN 
+                salaries ON bulletin.salarie_id = salaries.id
+            WHERE 
+                bulletin.periode LIKE :year
+            GROUP BY 
+            salaries.id, salaries.nom, salaries.prenom");
 
-$deduction1 = $doc->createElement('deduction');
-$deductions->appendChild($deduction1);
+            // Bind the year variable to the SQL statement
+            $stmt->execute(['year' => $year . '%']);
 
-$deduction1->appendChild($doc->createElement('type', 'ACOMPTE'));
-$deduction1->appendChild($doc->createElement('valeur', '3203'));
+            // Fetch all rows and print them
+            $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        // Prepare the SQL statement
+        $stmt = $pdo->prepare("SELECT 
+        salaries.id AS salarie_id, salaries.nom, salaries.prenom,salaries.numcafat,salaries.dnaissance,salaries.dembauche,
+        SUM(bulletin.brut) AS total_brut,
+        SUM(bulletin.nombre_heures) AS total_hours
+        FROM 
+            bulletin
+        INNER JOIN 
+            salaries ON bulletin.salarie_id = salaries.id
+        WHERE 
+            bulletin.periode LIKE :year
+        GROUP BY 
+        salaries.id, salaries.nom, salaries.prenom");
 
-$deduction2 = $doc->createElement('deduction');
-$deductions->appendChild($deduction2);
+        // Bind the year variable to the SQL statement
+        $stmt->execute(['year' => $year . '%']);
 
-$deduction2->appendChild($doc->createElement('type', 'RBS'));
-$deduction2->appendChild($doc->createElement('valeur', '100000'));
+        // Fetch all rows and print them
+        $employees = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 
-$decompte->appendChild($doc->createElement('montantAPayer', '1700000'));
 
-$filename = "output.xml";
+    // print_r($employees);
 
-// headers for download
-header('Content-Type: text/xml');
-header('Content-Disposition: attachment; filename="' . $filename . '"');
 
-echo $doc->saveXML();
-exit;
+    //Create XML
+    $doc = new DOMDocument('1.0', 'ISO-8859-1');
+    $doc->formatOutput = true;
+
+    // doc
+    $docElement = $doc->createElement('doc');
+    $doc->appendChild($docElement);
+
+    // entete
+    $entete = $doc->createElement('entete');
+    $docElement->appendChild($entete);
+
+    $entete->appendChild($doc->createElement('type', 'DN'));
+    $entete->appendChild($doc->createElement('version', 'VERSION_2_0'));
+    $entete->appendChild($doc->createElement('emetteur', $societe["enseigne"]));
+    $entete->appendChild($doc->createElement('dateGeneration', $now));
+
+    $logiciel = $doc->createElement('logiciel');
+    $entete->appendChild($logiciel);
+
+    $logiciel->appendChild($doc->createElement('editeur', 'MONEDITEUR'));
+    $logiciel->appendChild($doc->createElement('nom', 'MONPROGICIEL'));
+    $logiciel->appendChild($doc->createElement('version', '11.0.0 Patch 3'));
+    $logiciel->appendChild($doc->createElement('dateVersion', '2022-12-25'));
+
+    // corps
+    $corps = $doc->createElement('corps');
+    $docElement->appendChild($corps);
+
+    // periode
+    $periode = $doc->createElement('periode');
+    $corps->appendChild($periode);
+
+    $periode->appendChild($doc->createElement('type', $type));
+    $periode->appendChild($doc->createElement('annee', $yearOnly));
+    if ($month != "") {
+        $periode->appendChild($doc->createElement('numero', ltrim($month, "0")));
+    } elseif ($quarter != "") {
+        $periode->appendChild($doc->createElement('numero', $quarter));
+    } else {
+        $periode->appendChild($doc->createElement('numero', "1"));
+    }
+
+    // attributs
+    $attributs = $doc->createElement('attributs');
+    $corps->appendChild($attributs);
+
+    $attributs->appendChild($doc->createElement('complementaire', 'false'));
+    $attributs->appendChild($doc->createElement('contratAlternance', 'false'));
+    $attributs->appendChild($doc->createElement('pasAssureRemunere', 'false'));
+    $attributs->appendChild($doc->createElement('pasDeReembauche', 'false'));
+
+    // employeur
+    $employeur = $doc->createElement('employeur');
+    $corps->appendChild($employeur);
+
+    $employeur->appendChild($doc->createElement('numero', $societeNumeroCafat[0]));
+    $employeur->appendChild($doc->createElement('suffixe', ltrim($societeNumeroCafat[1], "0")));
+    $employeur->appendChild($doc->createElement('nom', $societe["enseigne"]));
+    $employeur->appendChild($doc->createElement('rid', $societeNumeroRidet[0]));
+    $employeur->appendChild($doc->createElement('codeCotisation', "001"));
+    $employeur->appendChild($doc->createElement('tauxATPrincipal', $societe["tauxat"]));
+
+    // assures
+
+    $assures = $doc->createElement('assures');
+    $corps->appendChild($assures);
+
+    foreach ($employees as $employee) {
+        $assure = $doc->createElement('assure');
+        $assures->appendChild($assure);
+
+        $assure->appendChild($doc->createElement('numero',  $employee['numcafat']));
+        $assure->appendChild($doc->createElement('nom', $employee['nom']));
+        $assure->appendChild($doc->createElement('prenoms', $employee['prenom']));
+        $assure->appendChild($doc->createElement('dateNaissance', $employee['dnaissance']));
+        $assure->appendChild($doc->createElement('codeAT', 'PRINCIPAL'));
+        $assure->appendChild($doc->createElement('etablissementRID', $societeNumeroRidet['1']));
+        if (isset($year)) {
+            $yearEmbauche = date("Y", strtotime($employee['dembauche']));
+            if ($yearEmbauche == $year) {
+                $assure->appendChild($doc->createElement('dateEmbauche', $employee['dembauche']));
+            }
+            if ($quarter != "") {
+                $yearEmbauche = date("Y-m", strtotime($employee['dembauche']));
+                if ($yearEmbauche == $year . "-" . "0" . $quarter) {
+                    $assure->appendChild($doc->createElement('dateEmbauche', $employee['dembauche']));
+                }
+            }
+            if ($month != "") {
+                $yearEmbauche = date("Ym", strtotime($employee['dembauche']));
+                if ($yearEmbauche == $year . $quarter) {
+                    $assure->appendChild($doc->createElement('dateEmbauche', $employee['dembauche']));
+                }
+            }
+        }
+        // $assure->appendChild($doc->createElement('codeCommune', $employee['']'09'));0
+        $assure->appendChild($doc->createElement('nombreHeures', $employee['total_hours']));
+        $assure->appendChild($doc->createElement('remuneration', $employee['total_brut']));
+    }
+
+
+    // assiettes
+    $assiettes = $doc->createElement('assiettes');
+    $assure->appendChild($assiettes);
+
+    $assiette1 = $doc->createElement('assiette');
+    $assiettes->appendChild($assiette1);
+
+    $assiette1->appendChild($doc->createElement('type', 'RUAMM'));
+    $assiette1->appendChild($doc->createElement('valeur', '500000'));
+
+    $assiette2 = $doc->createElement('assiette');
+    $assiettes->appendChild($assiette2);
+
+    $assiette2->appendChild($doc->createElement('type', 'FIAF'));
+    $assiette2->appendChild($doc->createElement('valeur', '500000'));
+
+    $assure->appendChild($doc->createElement('dateRupture', '2023-01-31'));
+    $assure->appendChild($doc->createElement('observations', 'FIN DE CONTRAT'));
+
+    // decompte
+    $decompte = $doc->createElement('decompte');
+    $corps->appendChild($decompte);
+
+    // cotisations
+    $cotisations = $doc->createElement('cotisations');
+    $decompte->appendChild($cotisations);
+
+    $cotisation = $doc->createElement('cotisation');
+    $cotisations->appendChild($cotisation);
+
+    $cotisation->appendChild($doc->createElement('type', 'RUAMM'));
+    $cotisation->appendChild($doc->createElement('tranche', 'TRANCHE_1'));
+    $cotisation->appendChild($doc->createElement('assiette', '3583400'));
+    $cotisation->appendChild($doc->createElement('valeur', '556144'));
+
+    $decompte->appendChild($doc->createElement('totalCotisations', '1803203'));
+
+    // deductions
+    $deductions = $doc->createElement('deductions');
+    $decompte->appendChild($deductions);
+
+    $deduction1 = $doc->createElement('deduction');
+    $deductions->appendChild($deduction1);
+
+    $deduction1->appendChild($doc->createElement('type', 'ACOMPTE'));
+    $deduction1->appendChild($doc->createElement('valeur', '3203'));
+
+    $deduction2 = $doc->createElement('deduction');
+    $deductions->appendChild($deduction2);
+
+    $deduction2->appendChild($doc->createElement('type', 'RBS'));
+    $deduction2->appendChild($doc->createElement('valeur', '100000'));
+
+    $decompte->appendChild($doc->createElement('montantAPayer', '1700000'));
+
+    $filename = "output.xml";
+
+    //Headers for download
+    header("Content-Type: text/xml");
+    // header('Content-Disposition: attachment; filename="' . $filename . '"');
+
+    echo $doc->saveXML();
+    exit;
+}
